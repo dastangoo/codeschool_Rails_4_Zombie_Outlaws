6c9b1415727fd9b568a83bdf4726a3c27c77dea9
dastangoo <hamed.dastangoo@gmail.com>
2017-12-04 15:01:42 +0330

    A Bloody Tale of Revenge

---
+++ testapp/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :items
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # match '/items/:id/purchase', to: 'items#purchase'
+  
+  # post '/items/:id/purchase', to: 'items#purchase'
+  
+  # match '/items/:id/purchase', to: 'items#purchase', via: :post
+  
+  match '/items/:id/purchase', to: 'items#purchase', via: :all
+end
